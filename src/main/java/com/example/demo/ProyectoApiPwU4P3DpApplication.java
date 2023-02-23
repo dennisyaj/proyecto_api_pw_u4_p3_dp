8d5a05f63ddee9e8027a66fc187874d6e96505f6
Dennisyaj <dennispilco@gmail.com>
2023-02-22 19:22:27 -0500

    first commit

---
+++ src/main/java/com/example/demo/ProyectoApiPwU4P3DpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoApiPwU4P3DpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoApiPwU4P3DpApplication.class, args);
+	}
+
+}
